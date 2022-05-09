12966341bf8a515aa1a36b6c5e46c2cba1c1de5d
Glayson <glaysonbss@gmail.com>
2022-05-09 15:09:13 -0300

    updates 5

---
+++ script.js
@@ -0,0 +1,13 @@
+// function rate_select() {
+//   const  = document.querySelectorAll('summary');
+
+//   for (let i = 0; i < summary.length; i++) {
+//     const el = summary[i];
+//     el.onclick = () => {
+//       for (let j = 0; j < summary.length; j++) {
+//         const color = summary[j] === el ? 'red' : 'inherit';
+//         summary[j].style.color = color;
+//       }
+//     }
+//   }
+// }
